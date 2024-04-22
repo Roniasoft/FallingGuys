e70eac47403c703518575396be1d590047c2509e
Mostafa <m.alizadeh91@gmail.com>
2024-04-22 13:01:47 +0200

    gameId added

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import App from "./App";
 import "./index.css";
 import { insertCoin } from "playroomkit"
 insertCoin({
-  skipLobby: true
+  skipLobby: true,
+  gameId: "8AINCsxKw26mY1HgEJ3V"
 }).then(() => {
   ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
